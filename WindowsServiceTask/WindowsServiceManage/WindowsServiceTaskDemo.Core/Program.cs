8d3d8aba4a9ecbaf4ecaa8189106a39c6aa88f78
toolgood <toolgood@qq.com>
2019-02-21 21:39:25 +0800

    + 添加.net core演示版本

---
+++ WindowsServiceTask/WindowsServiceManage/WindowsServiceTaskDemo.Core/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WindowsServiceTaskDemo.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigurationConstant.WIN_SERVICE_LOG_NAME = "WindowsServiceTaskDemo";
+            ConfigurationConstant.WIN_SERVICE_NAME = "WindowsServiceTaskDemo";
+            ConfigurationConstant.WIN_DISPLAY_NAME = "同步定时任务";
+            ConfigurationConstant.WIN_DESCRIPTION = "WindowsServiceTaskDemo";
+
+            QuartzHelper.RunArgs(args);
+        }
+    }
+}
